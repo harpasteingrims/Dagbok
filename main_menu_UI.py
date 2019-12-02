2aba535e61654b5692dbd36b327edacc3fe626d4
harpasteingrims <54943088+harpasteingrims@users.noreply.github.com>
2019-12-02 22:03:43 +0000

    Update main_menu_UI.py

--- main_menu_UI.py
+++ main_menu_UI.py
@@ -1,8 +1,8 @@
-from EmployeeUI import Employees_UI
-from VoyageUI import Voyages_UI
-from DestinationUI import Destinations_UI
-form AirplaneUI import Airplane_UI
-from Information_about_a_date import IAAD_UI
+from EmployeeUI import EmployeesUI
+from VoyageUI import VoyagesUI
+from DestinationUI import DestinationsUI
+form AirplaneUI import AirplaneUI
+from Information_about_a_date import IAADUI
 
 class Main_menu():
 
@@ -14,23 +14,23 @@ class Main_menu():
         print()
 
         if action == "1":
-            Employees_UI.show_employee_menu()
+            EmployeesUI.show_employee_menu()
             pass
 
         elif action == "2":
-            Voyages_UI.show_destination_menu()
+            VoyagesUI.show_destination_menu()
             pass
 
         elif action == "3":
-            Destinations_UI.show_destination_menu()
+            DestinationsUI.show_destination_menu()
             pass
 
         elif action == "4":
-            Airplane_UI.show_airplane_menu()
+            AirplaneUI.show_airplane_menu()
             pass
 
         elif action == "5":
-            IAAD_UI.show_enter_date()
+            IAADUI.show_enter_date()
             pass
             
 def main():
