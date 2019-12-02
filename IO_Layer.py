e104f0dcc86e3cc90bbc27940b72304d0acf2965
harpasteingrims <54943088+harpasteingrims@users.noreply.github.com>
2019-12-02 10:25:34 +0000

    Update IO_Layer.py

--- IO_Layer.py
+++ IO_Layer.py
@@ -6,43 +6,43 @@ class GetIO:
     def __init__(self, filename):
         IOAPI.__init__(self, filename)
 
-    def get_employee():                     #Retrieves employees and sends to Get LL
+    def get_employee():                     '''Retrieves employees and sends to Get LL'''
         pass
 
-    def get_airplane():                     #Retrieves airplanes and sends to Get LL
+    def get_airplane():                     '''Retrieves airplanes and sends to Get LL'''
         pass
         
-    def get_destinations():                 #Retrieves destinations and sends to Get LL
+    def get_destinations():                 '''Retrieves destinations and sends to Get LL'''
         pass
 
-    def get_voyage():                       #Retrieves voyages and sends to Get LL
+    def get_voyage():                       '''Retrieves voyages and sends to Get LL'''
         pass
 
 class CreateIO:
     def __init__(self, filename):          
         IOAPI.__init__(self, filename)
 
-    def add_employee():                     #Retrieves employees and sends to Create LL
+    def add_employee():                     '''Retrieves employees and sends to Create LL'''
         pass
 
-    def add_airplane():                     #Retrieves airplanes and sends to Create LL
+    def add_airplane():                     '''Retrieves airplanes and sends to Create LL'''
         pass
         
-    def add_destination():                  #Retrieves destinations and sends to Create LL
+    def add_destination():                  '''Retrieves destinations and sends to Create LL'''
         pass
 
-    def add_voyage():                       #Retrieves voyages and sends to Create LL
+    def add_voyage():                       '''Retrieves voyages and sends to Create LL
         pass
     
 class UpdateIO:
     def __init__(self, filename):
         IOAPI.__init__(self, filename)
 
-    def update_employee():                  #Retrieves employees and sends to Update LL
+    def update_employee():                  '''Retrieves employees and sends to Update LL'''
         pass
 
-    def update_destination():               #Retrieves destinations and sends to Update LL
+    def update_destination():               '''Retrieves destinations and sends to Update LL'''
         pass
 
-    def update_voyage():                    #Retrieves voyages and sends to Update LL
+    def update_voyage():                    '''Retrieves voyages and sends to Update LL'''
         pass
