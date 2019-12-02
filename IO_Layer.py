76aac7b4cd0c8244a1a36ec066455a3f69daf791
harpasteingrims <54943088+harpasteingrims@users.noreply.github.com>
2019-12-02 10:19:42 +0000

    Update IO_Layer.py

--- IO_Layer.py
+++ IO_Layer.py
@@ -6,43 +6,43 @@ class GetIO:
     def __init__(self, filename):
         IOAPI.__init__(self, filename)
 
-    def get_employee():
+    def get_employee():                     #Retrieves employees and sends to Get LL
         pass
 
-    def get_airplane():
+    def get_airplane():                     #Retrieves airplanes and sends to Get LL
         pass
         
-    def get_destinations():
+    def get_destinations():                 #Retrieves destinations and sends to Get LL
         pass
 
-    def get_voyage():
+    def get_voyage():                       #Retrieves voyages and sends to Get LL
         pass
 
 class CreateIO:
-    def __init__(self, filename):
+    def __init__(self, filename):          
         IOAPI.__init__(self, filename)
 
-    def add_employee():
+    def add_employee():                     #Retrieves employees and sends to Create LL
         pass
 
-    def add_airplane():
+    def add_airplane():                     #Retrieves airplanes and sends to Create LL
         pass
         
-    def add_destination():
+    def add_destination():                  #Retrieves destinations and sends to Create LL
         pass
 
-    def add_voyage():
+    def add_voyage():                       #Retrieves voyages and sends to Create LL
         pass
     
 class UpdateIO:
     def __init__(self, filename):
         IOAPI.__init__(self, filename)
 
-    def update_employee():
+    def update_employee():                  #Retrieves employees and sends to Update LL
         pass
 
-    def update_destination():
+    def update_destination():               #Retrieves destinations and sends to Update LL
         pass
 
-    def update_voyage():
+    def update_voyage():                    #Retrieves voyages and sends to Update LL
         pass
