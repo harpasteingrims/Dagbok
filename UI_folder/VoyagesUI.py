203f2a8546881bfdaf4d3a326f291f043f1faa79
harpasteingrims <54943088+harpasteingrims@users.noreply.github.com>
2019-12-03 15:52:25 +0000

    Update VoyagesUI.py

--- UI_folder/VoyagesUI.py
+++ UI_folder/VoyagesUI.py
@@ -3,69 +3,63 @@ class VoyagesUI():
     def __init__(self):
         pass
     
-    @classmethod
-    def show_overview_voyage(cls):
+    def show_overview_voyage(self):
         """ This prints the overview of all voyages """
-        print("\n{}".format(cls.LENGTH_STAR*"*"))
+        print("\n{}".format(self.LENGTH_STAR*"*"))
         print("OVERVIEW OF VOYAGES")
         print("\nflugvel lala")
 
-    @classmethod
-    def show_voyage_menu(cls):
+    def show_voyage_menu(self):
         """ This prints out the voyage menu """
-        print(cls.LENGTH_STAR*"*")
+        print(self.LENGTH_STAR*"*")
         print("VOYAGES")
         print()
         print("1 Print overview of voyages\n2 Create a voyage\n3 Assign crew to flights\nB Back")
         print()
         action_str = input("Choose action: ")
         if action_str == "1":
-            cls.show_overview_voyage()
+            self.show_overview_voyage()
         elif action_str == "2":
-            cls.show_create_voyage_menu()
+            self.show_create_voyage_menu()
         elif action_str == "3":
-            cls.show_not_staffed_voyages()
+            self.show_not_staffed_voyages()
         elif action_str == "B" or action_str == "b":
             from MainmenuUI import MainmenuUI
             MainmenuUI.show_main_menu()
 
-    @classmethod
-    def show_assign_staff_form(cls):
+    def show_assign_staff_form(self):
         """ This prints out the form to assign a staff to a voyage """
-        print("\n{}".format(cls.LENGTH_STAR*"*"))
+        print("\n{}".format(self.LENGTH_STAR*"*"))
         print("ASSIGN CREW TO VOYAGES")
-        cls.show_not_staffed_voyages()
+        self.show_not_staffed_voyages()
         print("B Back")
         print()
         voyage_str = input("Choose a voyage")
         #Þurfum meira hér til að klára þetta fall
-    
-    @classmethod
-    def show_create_voyage_menu(cls):
+
+    def show_create_voyage_menu(self):
         """ This prints out the menu for create a voyage """
-        print("\n{}".format(cls.LENGTH_STAR*"*"))
+        print("\n{}".format(self.LENGTH_STAR*"*"))
         print("CREATE A VOYAGE")
         print()
         print("1 See common voyages\n2 Create a voyage manually\nB Back")
         print()
         action_str = input("Choose action: ")
         if action_str == "1":
-            cls.show_see_common()
+            self.show_see_common()
         elif action_str == "2":
-            cls.show_create_manually_form()
+            self.show_create_manually_form()
         elif action_str == "B" or action_str == "b":
-            cls.show_create_voyage_menu()
+            self.show_create_voyage_menu()
     
-    @classmethod
-    def show_see_common(cls):
+    def show_see_common(self):
         """ This prints out all the common voyages """
-        print("\n{}".format(cls.LENGTH_STAR*"*"))
+        print("\n{}".format(self.LENGTH_STAR*"*"))
         print("SEE COMMON VOYAGES")
 
-    @classmethod
-    def show_create_manually_form(cls):
+    def show_create_manually_form(self):
         """ This prints out the form to add a voyage manually """
-        print("\n{}".format(cls.LENGTH_STAR*"*"))
+        print("\n{}".format(self.LENGTH_STAR*"*"))
         print("CREATE A VOYAGE MANUALLY")
         print("\nDate")
         voyage_date = input("Enter outbound departure date(dd/mm/yy): ")
@@ -82,39 +76,25 @@ class VoyagesUI():
         print()
         action_str = input("Choose action: ")
         if action_str == "1":
-            cls.show_assign_staff_form()
+            self.show_assign_staff_form()
         elif action_str == "S" or action_str == "s":
             print("\n*Voyage successfully created*")
-            cls.show_create_voyage_menu()
+            self.show_create_voyage_menu()
             pass #Hérna þurfum við að skella þessu í lista/dictionary og svo fara einn til baka eða lenda aftur á þessum skjá
         elif action_str == "B" or action_str == "b":
-            cls.show_create_voyage_menu()
+            self.show_create_voyage_menu()
 
-    @classmethod
-    def show_available_airplane(cls):
+    def show_available_airplane(self):
         """ This prints out all the available airplanes for a voyage """
         pass
     
-    @classmethod
-    def show_time_from_iceland(cls):
+    def show_time_from_iceland(self):
         """ This prints the flight time from Iceland for the flight """
         pass 
 
-    @classmethod
-    def show_date_from_iceland(cls):
+    def show_date_from_iceland(self):
         """ This prints what date is for the flight """
         pass 
-
-    @classmethod
-    def show_destinations(cls):
-        """ This prints out all the destinations """
-        pass 
-
-    @classmethod
-    def show_not_staffed_voyages(cls):
-        """ This prints out all the not fully staffed voyages that are available """
-        #Vantar meira hér til þess að klára
-
 
     def show_destinations(self):
         """ This prints out all the destinations """
@@ -125,6 +105,3 @@ class VoyagesUI():
         #Vantar meira hér til þess að klára
 
 
-
-lala = VoyagesUI()
-lala.show_menu_voyage()
