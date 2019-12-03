15c4d21a0201d4d94d290cfa7e190c45382e851d
ninamargret <54943095+ninamargret@users.noreply.github.com>
2019-12-03 15:41:37 +0000

    Update MainmenuUI.py

--- UI_folder/MainmenuUI.py
+++ UI_folder/MainmenuUI.py
@@ -1,41 +1,41 @@
 from EmployeesUI import EmployeesUI
-from VoyagesUI import VoyagesUI
-from DestinationsUI import DestinationsUI
-from AirplanesUI import AirplaneUI
-from IAADUI import IAADUI
+#from VoyageUI import Voyages_UI
+#from DestinationUI import Destinations_UI
+#form AirplaneUI import Airplane_UI
+#from Information_about_a_date import IAAD_UI
 
 class MainmenuUI():
-    lenght_star = 20
+    LENGTH_STAR = 20
 
-    @classmethod
-    def show_main_menu(cls):
+    def show_main_menu(self):
 
-        print("*"*cls.lenght_star)
+        print("*" * self.LENGTH_STAR)
         print("MAIN MENU\n\n1 Employees\n2 Voyages\n3 Destinations\n4 Airplanes\n5 Search a date\n")
         
         action = input("Choose action: ")
         print()
 
         if action == "1":
-            EmployeesUI.show_employee_menu()
+            EmployeesUI.show_employee_menu(self)
 
         elif action == "2":
-            VoyagesUI.show_voyage_menu()
+            Voyages_UI.show_voyage_menu()
             pass
 
         elif action == "3":
-            DestinationsUI.show_destination_menu()
+            Destinations_UI.show_destination_menu()
             pass
 
         elif action == "4":
-            AirplanesUI.show_airplane_menu()
+            Airplane_UI.show_airplane_menu()
             pass
 
         elif action == "5":
-            IAADUI.show_enter_date()
+            IAAD_UI.show_enter_date()
             pass
             
 def main():
-    Main_menu.show_main_menu()
+    menu = MainmenuUI()
+    menu.show_main_menu()
 
 main()
