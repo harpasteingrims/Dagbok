e8a4ac2aee96b4837cee59c50f3bdeb59eee322c
Harpa Steingrimsdottir <harpasteingrims@gestur-325.ru.is>
2019-12-03 16:34:01 +0000

    breytti meira i iaadui

--- UI_folder/IAADUI.py
+++ UI_folder/IAADUI.py
@@ -6,61 +6,61 @@ class IAADUI():
     def __init__(self):
         pass
 
-    def show_enter_date(cls):
+    def show_enter_date(self):
         print("\n")
         user_input_date = input("Enter a date: ")
         print("\n")
-        cls.show_IAAD_menu()
+        self.show_IAAD_menu()
         """ This prints out, input date """
         pass
 
-    def show_available_employees(cls):
+    def show_available_employees(self):
         """ This prints out the available employees from a certain date """
         print("this is a list of available employees")
         print("\n")
         print("B Back")
-        user_input = cls.choose_action()
+        user_input = self.choose_action()
         if user_input == "b":
             IAADUI.show_IAAD_menu()
         pass
 
-    def show_unavailable_employees(cls):
+    def show_unavailable_employees(self):
         """ This prints out the unavailable employees from a certain date """
         print("this is a list of unavailable employees")
         print("\n")
         print("B Back")
-        user_input = cls.choose_action()
+        user_input = self.choose_action()
         if user_input == "b":
             IAADUI.show_IAAD_menu()
         pass
 
-    def show_airplane_status(cls):
+    def show_airplane_status(self):
         """ This prints out the status of a airplane from a certain date """
         print("this is a list of airplanes and their status")
         print("\n")
         print("B Back")
-        user_input = cls.choose_action()
+        user_input = self.choose_action()
         if user_input == "b":
             IAADUI.show_IAAD_menu()
         pass
    
-    def show_voyages_status(cls):
+    def show_voyages_status(self):
         """ This prints out the status of a voyage from a certain date """
         print("this is a list of voyages and their status")
         print("\n")
         print("B Back")
-        user_input = cls.choose_action()
+        user_input = self.choose_action()
         if user_input == "b":
             IAADUI.show_IAAD_menu()
         pass
 
 
-    def show_IAAD_menu(cls):
+    def show_IAAD_menu(self):
         """ This prints out the information about a date menu """
         print("IAAD")
         print("1 Available Employees","\n2 Unavailable Employees","\n3 Status of voyages","\n4 Status of Airplanes","\nB Back")
 
-        user_input = cls.choose_action()
+        user_input = self.choose_action()
         if user_input == "1":
             IAADUI.show_available_employees()
         elif user_input == "2":
@@ -74,7 +74,7 @@ class IAADUI():
             Main_menu.show_main_menu()
             
   
-    def choose_action(cls):
+    def choose_action(self):
         print("\n")
         user_action = input("Choose action: ")
         return user_action
