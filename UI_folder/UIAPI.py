0079591ff9b2f7c870d064bff9b811deb60783a7
ninamargret <54943095+ninamargret@users.noreply.github.com>
2019-12-03 15:44:49 +0000

    Create UIAPI.py

---
+++ UI_folder/UIAPI.py
@@ -0,0 +1,13 @@
+class UIAPI():
+
+    def get_employee_overwiew(self):
+        pass
+
+    def get_pilot_overview(self):
+        pass
+
+    def get_cabin_crew_overwiew(self):
+        pass
+    
+    def get_pilot_by_name(self):
+        pass
