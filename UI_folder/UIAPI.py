74e8b51a9632c1ede68327efcf0bd1ca78cf49c3
Harpa Steingrimsdottir <harpasteingrims@gestur-285.ru.is>
2019-12-04 16:16:35 +0000

    sdkjf

--- UI_folder/UIAPI.py
+++ UI_folder/UIAPI.py
@@ -2,36 +2,12 @@ import sys
 sys.path.insert(1, '~/VERKLEGT-1-verkefni/')
 
 from LL_folder.LLAPI import LLAPI
-from UI_folder.EmployeesUI import EmployeesUI
-from UI_folder.VoyagesUI import VoyagesUI
-from UI_folder.DestinationsUI import DestinationsUI
-from UI_folder.AirplanesUI import AirplanesUI
-from UI_folder.IAADUI import IAADUI
-class UImanager():
+class UIMNG():
     
     def __init__(self):
-        self.llapi = LLAPI()
-        self.employee = EmployeesUI()
-        self.voyages = VoyagesUI()
-        self.destination = DestinationsUI()
-        self.airplanes = AirplanesUI()
+        self.__voyage_repo = LLAPI()
 
-    def MainmenuUI(self):
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
     ''' EMPLOYEES '''    
     def get_employee_overwiew(self):
         pass
@@ -59,26 +35,13 @@ class UImanager():
     def get_voyages_overview(self):
         return self.__voyage_repo.get_voyages()
 
+
     #__init__(self):
         #self.llayer = LLAPI()
         #self.mainmenu = MainmenuUI()
         #...
 
 
-#Þetta er rotarskrain
 
-# Arnar: látið þessa skrá bara heita __main__.py og hafið hana ekki
-# í sér möppu heldur í möppunni sem ER raunverulega rótin á verkefninu
-# (þeas í möppunni  VERKLEGT-1-verkefni)
-
-
-from UI_folder.MainmenuUI import MainmenuUI
-
-def main():
-    ui = MainmenuUI()
-    ui.show_main_menu()
-
-if __name__ == '__main__':
-    main()
 
     
