5b8a29165e49f6d230a814a478ad4de58a19baaf
viktoriainga <viktoriainga@gmail.com>
2019-12-04 15:58:17 +0000

    gerdi tilvik i llapi -viktoria

--- LL_folder/LLAPI.py
+++ LL_folder/LLAPI.py
@@ -1,13 +1,18 @@
+from IOAPI.get_employee import get_employee
 class LLAPI():
+    def __init__(self):
+        self.IOAPI = IOAPI()
+        self.createll = getcreatell
+        self.updatell = getupdatell
+        self.getvoyages = getvoyages
+        self.getairplanes = getairplanes
+        self.getdestinations = getdestinations
+        
 
-    def get_employee_name(self):
-        pass
+
 
     def add_employee(self):
         pass
 
     def get_employee(self):
-        pass #Listar allar employees
-
-    def get_voyages(self):
-        pass
+        pass #Listar allar employeea
