e9cd5b8c821af29d2cc8a37f7fb4d23050553aa6
arnaringig <arnaringigunnarsson@icloud.com>
2019-12-04 00:04:53 +0000

    Leiðbeiningar

--- Rotar_folder/rotin.py
+++ Rotar_folder/rotin.py
@@ -1,4 +1,11 @@
 #Þetta er rotarskrain
+
+
+# Arnar: látið þessa skrá bara heita __main__.py og hafið hana 
+# ekki í sér möppu heldur í möppunni sem ER raunverulega rótin á verkefninu
+# (þeas í möppunni  VERKLEGT-1-verkefni)
+
+
 from UI_folder.MainmenuUI import MainmenuUI
 
 def main():
