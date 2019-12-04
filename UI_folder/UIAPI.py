d07137a9f246580743796c9a7f721b3e1f81ab0b
viktoriainga <viktoriainga@gmail.com>
2019-12-04 10:30:31 +0000

    reyndi ad byrja a UIAPI - viktoria

--- UI_folder/UIAPI.py
+++ UI_folder/UIAPI.py
@@ -1,5 +1,15 @@
 class UIAPI():
+    ''' DESTINATIONS '''
+    def get_destination_overview(self):
+        # fall fyrir show_destination_overview, í því falli á að prenta út overview af destinations
+        #kalla á LL-layer klasann sem er með lista af destinations
+        #return
+    def new_destination(self):
+        # fall fyrir show_create_des_form, þarf að senda nýja 
 
+
+
+    ''' EMPLOYEES '''
     def get_employee_overwiew(self):
         pass
 
