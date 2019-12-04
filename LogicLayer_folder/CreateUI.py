7c0cf6794fa2ab92b29a200e9a357a9e46efd8f1
hildurd99 <54947029+hildurd99@users.noreply.github.com>
2019-12-04 10:26:11 +0000

    Update CreateUI.py

---
+++ LogicLayer_folder/CreateUI.py
@@ -0,0 +1,34 @@
+class create_LL(LLAPI):
+    '''Subclass of LLAPI that is only designed to create something'''
+    def __init__(self):
+        pass
+
+    def create_pilot():
+        '''Method that creates a new pilot'''
+        new_pilot = {}
+        pass
+
+    def create_cabincrew():
+        '''Method that creates a new cabincrew member'''
+        new_cabincrew = {}
+        pass
+    
+    def create_voyage():
+        '''Method that creates a new voyage'''
+        new_voyage = {}
+        pass
+
+    def create_destination():
+        '''Method that creates a new destination'''
+        new_destination = {}
+        pass
+    
+    def create_airplanes():
+        '''Method that creates a new airplane'''
+        new_airplane = {}
+        pass
+    
+    def create_common_voyages():
+        '''Method that creates common voyages'''
+        common_voyages = []
+        pass
