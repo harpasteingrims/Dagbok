8225dbeb20b70ea0c8527ba923138316f4e58efa
viktoriainga <viktoriainga@gmail.com>
2019-12-04 12:54:23 +0000

    gerdi AirplaneModel - vikka

---
+++ models/AirplanesModel.py
@@ -0,0 +1,12 @@
+class AirplanesModel():
+    def __init__(self, ID, airplane_type, manufacturer, seat_amount):
+        self.ID = ID
+        self.airplane_type = airplane_type
+        self.manufacturer = manufacturer
+        self.seat_amount = seat_amount
+    
+    def set_airplane(self):
+        airplane = {}
+        airplane[self.ID] = [self.airplane_type, self.manufacturer, self.seat_amount]
+
+        return airplane
