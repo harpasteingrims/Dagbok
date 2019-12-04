271a5143fb24724362b9840c4b6b5471f4737d44
Harpa Steingrimsdottir <harpasteingrims@gestur-285.ru.is>
2019-12-04 11:52:11 +0000

    eh

--- models/VoyagesModel.py
+++ models/VoyagesModel.py
@@ -1,3 +1,15 @@
 class VoyagesModel():
     def __init__(self, date, time, destination, airplane):
+        self.date = date
+        self.time = time
+        self.destination = destination
+        self.airplane = airplane
+
+    def get_voyages(self):
+        pass
+
+    def set_voyages(self):
+        pass
+
+    def add_voyages(self):
         pass
