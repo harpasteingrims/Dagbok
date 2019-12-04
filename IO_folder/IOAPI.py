de7b38041cb69ee4987eabbf1009b55f5205e124
godi15 <godi15@ru.is>
2019-12-04 16:44:11 +0000

    jkdd

--- IO_folder/IOAPI.py
+++ IO_folder/IOAPI.py
@@ -1,21 +1,4 @@
-
-
 class IOAPI():
-    def __init__(self, filename):
+    def __init__(self):
         self.filename = filename
     
-class GetIO:
-    def __init__(self, filename):
-        IOAPI.__init__(self, filename)
-
-    def get_employee():                     '''Retrieves employees and sends to Get LL'''
-        pass
-
-    def get_airplane():                     '''Retrieves airplanes and sends to Get LL'''
-        pass
-        
-    def get_destinations():                 '''Retrieves destinations and sends to Get LL'''
-        pass
-
-    def get_voyage():                       '''Retrieves voyages and sends to Get LL'''
-        pass
