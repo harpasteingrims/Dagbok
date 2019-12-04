a774f7a64acd78c9234eddf1a40a533237f34a29
Hallmar <schramhallmar@gmail.com>
2019-12-04 11:53:18 +0000

    bætti í getIO

--- Data_folder/getIO.py
+++ Data_folder/getIO.py
@@ -30,6 +30,7 @@ class GetIO():
         airplane_file = open("Aircraft.csv")
 
         airplane_file_list = []
+
         for line in airplane_file:
             line = line.strip().split(",")
             PlaneID = line[0]
@@ -44,11 +45,36 @@ class GetIO():
         airplane_list = []
         for obj in airplane_file_list[1:]:
             airplane_list.append(obj)
-        print(airplane_list)
 
         return airplane_list
-    
+    def get_destinations():
+        dest_file = open("destinations.csv")
+        
+        dest_file_list = []
+        counter = 1
+        for line in dest_file:
+            if counter = 1:
+                counter += 1
+            else:
+                line = line.strip.split(",")
+                country = line[0]
+                airport = line[1]
+                flight_dur_from_Ice = line[2]
+                dist_from_Ice = line[3]
+                contact_name = line[4]
+                contact_phone_number = line[5]
+            
+            
+
+            DestinationsModel.set_destinations(country,airport,flight_dur_from_Ice,dist_from_Ice,contact_name,contact_phone_number)
+            dest_file_list.append(destination)
+        destination_list = []
+        for obj in dest_file_list[1:]:
+            destination_list.append(obj)
+
+
+
 def main():
-    listi = GetIO.get_airplane()
+    listi = GetIO.get_destinations()
     pass
 main()
