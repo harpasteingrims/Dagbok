573e962c1fb3c3e721a22f9a372082c2f6cc488e
godi15 <godi15@ru.is>
2019-12-05 14:45:23 +0000

    jdjd

--- LL_folder/GetEmployeesLL.py
+++ LL_folder/GetEmployeesLL.py
@@ -5,29 +5,49 @@ class GetEmployeesLL():
         self.unavaliable_employees = []
         self.pilot_list = []
         
-    def get_all_employees(self):
+    def list_all_employees(self):
         """ Calls the IOAPI to get a list of all employees """
         return sorted(self.ioapi.get_list_of_all_employees())
 
-    def get_all_pilots(self):
+    def list_all_pilots(self):
         """ Calls the IOAPI to get a list of all pilots """
         return sorted(self.ioapi.get_list_of_all_pilots())
     
-    def get_all_cabin_crew(self):
+    def list_all_cabin_crew(self):
         """ Calls the IOAPI to get a list of the whole cabin crew """
         return sorted(self.ioapi.get_list_of_all_cabin_crew())
     
-    def get_info_about_pilot_by_name(self, name):
-        """"""
+    def find_common_named_pilots(self, name):
+        common_names
+        self.pilot_list = self.get_all_pilots()
+        for pilot_object in self.pilot_list:
+            pass
+
+    def list_info_about_pilot_by_name(self, name):
+        """  """
+        searched_pilot_info = []
 
         self.pilot_list = self.get_all_pilots()
 
+        for pilot in self.pilot_list:
+            if pilot.name == name:
+                pass
+
+    def list_info_about_cabincrew_by_name(self):
+        pass
         
 
-    def get_info_about_cabincrew_by_name(self):
+    def list_info_about_cabincrew_by_name(self):
         pass
 
-    def get_available_employees(self,date):
+
+    def list_schedule_cabincrew_by_date(self):
+        pass
+
+    def list_schedule_pilot_by_date(self):
+            pass
+
+    def list_available_employees(self,date):
         
         """ Calls the IOAPI to get a list of all employees """
         all_employee_list = self.get_all_employees()
@@ -35,7 +55,7 @@ class GetEmployeesLL():
         # flokka þá sem eru að vinna á ákv dagsetningu.......
         return avaliable_employees
 
-    def get_unavailable_employees(self,date):
+    def list_unavailable_employees(self,date):
         
         
         #all_employee_list = self.ioapi.get_list_of_all_employees()
@@ -44,4 +64,14 @@ class GetEmployeesLL():
 
         return unavaliable_employees
 
+    def list_unavaliable_pilots(self):
+        pass
+
+    def list_available_cabin_crew(self):
+        pass
     
+    def list_available_cabin_crew(self):
+        pass
+
+    def list_unavailable_cabin_crew(self):
+        pass
