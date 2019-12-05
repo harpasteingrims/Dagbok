ace76fa0056f7d7cf6280c6d55bdd52856fb8677
Harpa Steingrimsdottir <harpasteingrims@Harpas-MacBook.lan>
2019-12-05 20:24:18 +0000

    laga

--- models/PilotModel.py
+++ models/PilotModel.py
@@ -3,12 +3,11 @@ class PilotsModel():
         self.SSN = SSN 
         self.name = name
         self.role = role
-        self.rank = 
+        self.rank = rank
         self.license_type = license_type
         self.address = address
         self.mobile_number = mobile_number
         self.email = email
         
     def to_csv_string(self):
-        return f"{self.ssn},{self.name},{self.role},{self.rank},{self.license_type},{self.address},{self.mobile_number},{self.email}"
-
+        return f"{self.SSN},{self.name},{self.role},{self.rank},{self.license_type},{self.address},{self.mobile_number},{self.email}"
