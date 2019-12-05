a316f71bda16eabe35fdb1eb5964007546eb3212
viktoriainga <viktoriainga@gmail.com>
2019-12-05 14:42:33 +0000

    jd

--- IO_folder/CreateIO.py
+++ IO_folder/CreateIO.py
@@ -1,29 +1,24 @@
-from IO_folder.getIO import GetIO
+from IO_folder.GetIO import GetIO
 from models.CabinCrewModel import CabinCrewModel
 from models.PilotModel import PilotsModel
 from models.AirplanesModel import AirplanesModel
+import csv
 
 class CreateIO():
     def __init__(self):
         pass
     
-    def add_pilot(self):
-        GetIO.get_all_pilots(self)
-        
-        #hér er ég að sækja listann af employees sem get er búinn að búa til
-        #IOAPI.get_employee(self)
+    def add_pilot(self, new_pilot_list):
+        with open(r'Piots.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(new_pilot_list)
 
-        #vantar að setja kannski id number 
     
     def add_cabincrew(self):
-        cabincrew_list = getIO.get_cabincrew()
-        cabincrew = models.CabinCrewModel()
-        cabincrew_list.append(cabincrew)
+
         
     def add_airplane(self):
-        airplane_list = DataLayer_folder.GetIO()
-        airplane = models.AirplanesModel()
-        airplane_list.append(airplane)
+
 
     def add_destiantions(self):
         pass
