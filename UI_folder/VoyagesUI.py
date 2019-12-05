b5603bbedfba8cfea6e41cceaa0070eb561df979
Hildur <hildurdav@hotmail.com>
2019-12-05 14:19:17 +0000

    l

--- UI_folder/VoyagesUI.py
+++ UI_folder/VoyagesUI.py
@@ -6,18 +6,12 @@ class VoyagesUI():
     def __init__(self, llapi):
         self.llapi = llapi
     
-    def show_overview_voyage(self):
-        """ This prints the overview of all voyages """
-        print("\n{}".format(self.LENGTH_STAR*"*"))
-        print("OVERVIEW OF VOYAGES")
-        voyages = self.llapi.get_voyages_overview() #Kallar á fall i llapanum sem returnar öllum vinnuferðum
-        print(voyages)
-
     def show_voyage_menu(self):
-        """ This prints out the voyage menu """
+        """This prints the voyage menu"""
+
         print(self.LENGTH_STAR*"*")
         print("VOYAGES \n\n1 Print overview of voyages\n2 Create a voyage\n3 Assign crew to flights\nB Back")
-        print()
+
         action_str = input("Choose action: ").lower()
         if action_str == "1":
             self.show_overview_voyage()
@@ -28,24 +22,18 @@ class VoyagesUI():
         elif action_str == "b":
             return
 
-
-    def show_assign_staff_form(self):
-        """ This prints out the form to assign a staff to a voyage """
-        print("\n{}".format(self.LENGTH_STAR*"*"))
-        print("ASSIGN CREW TO VOYAGES")
-        print("\nB Back") #Kannski sleppa
-        #Listi yfir alla lausa pilots og þar þarf IO og fá date til að sjá hverjir eru lausar þennan dag
-        pilot = input("Enter a pilot: ")
-        #Listi yfir alla lausa cabincrew
-        cabincrew = input("Enter a cabincrew: ")
+    def show_overview_voyage(self):
+        """This prints the overview of all voyages"""
         
-        print()
-        
-        #Þurfum meira hér til að klára þetta fall
-
+        print(self.LENGTH_STAR*"*")
+        print("OVERVIEW OF VOYAGES")
+        voyages = self.llapi.get_voyages_overview() #Kallar á fall i llapanum sem returnar öllum vinnuferðum
+        print(voyages)
+    
     def show_create_voyage_menu(self):
-        """ This prints out the menu for create a voyage """
-        print("\n{}".format(self.LENGTH_STAR*"*"))
+        """This prints the menu for create a voyage"""
+        
+        print(self.LENGTH_STAR*"*")
         print("CREATE A VOYAGE \n\n1 See common voyages\n2 Create a voyage manually\nB Back")
         print()
         action_str = input("Choose action: ").lower()
@@ -54,26 +42,30 @@ class VoyagesUI():
         elif action_str == "2":
             self.show_create_manually_form()
         elif action_str == "b":
-            self.show_create_voyage_menu()
-    
+            self.show_voyage_menu()
+
     def show_see_common(self):
-        """ This prints out all the common voyages """
-        print("\n{}".format(self.LENGTH_STAR*"*"))
+        """This prints all the common voyages"""
+        
+        print(self.LENGTH_STAR*"*")
         print("SEE COMMON VOYAGES")
         self.llapi.get_common_voyages() #Þessi listi þarf að vera númeraður
         common_voyage = input("Choose a common voyage: ") #Þetta fer inní create a common voyage fallið
+        #Hér þarf að vera einhver counter kóði eins og í emergency contact destinations :/
 
     def create_a_common_voyage(self):
-        """ This creates a voyage from the common voyages but with a new date and a new id """
-        print("\n{}".format(self.LENGTH_STAR*"*"))
+        """This creates a voyage from the common voyages but with a new date and a new id"""
+        
+        print(self.LENGTH_STAR*"*")
         print("INPUT DEPARTURE DATE AND ARIPLANE ID")
         departure_date = input("Enter outbound departure date: dd/mm/yy: ")
         airplane_id = input("Enter airplane ID: ")
         pass #Eftir að klára þetta :)
 
     def show_create_manually_form(self): #Lista upp alla áfangastaði allar tímasetningar sem eru uppteknar allar flugvélar sem eru lausar
-        """ This prints out the form to add a voyage manually """
-        print("\n{}".format(self.LENGTH_STAR*"*"))
+        """This prints the form to add a voyage manually"""
+        
+        print(self.LENGTH_STAR*"*")
         print("CREATE A VOYAGE MANUALLY")
         print("\nDate")
         voyage_date = input("Enter outbound departure date(dd/mm/yy): ")
@@ -103,8 +95,25 @@ class VoyagesUI():
         elif action_str == "b":
             self.show_create_voyage_menu()
 
+    def show_assign_staff_form(self):
+        """This prints the form to assign a staff to a voyage"""
+        
+        print(self.LENGTH_STAR*"*")
+        print("ASSIGN CREW TO VOYAGES")
+        print("\nB Back") #Kannski sleppa
+        #Listi yfir alla lausa pilots og þar þarf IO og fá date til að sjá hverjir eru lausar þennan dag
+        pilot = input("Enter a pilot: ")
+        #Listi yfir alla lausa cabincrew
+        cabincrew = input("Enter a cabincrew: ")
+        
+        print()
+        
+        #Þurfum meira hér til að klára þetta fall
+
     def show_not_staffed_voyages(self):
-        """ This prints out all the not fully staffed voyages that are available """
+        """This prints all the not fully staffed voyages that are available"""
+        
+        print(self.LENGTH_STAR*"*")
         not_staffed = self.llapi.get_not_staffed_voyages() #Þessi listi þarf að vera númeraður
         print(not_staffed)
         voyage_str = input("Choose a voyage")
