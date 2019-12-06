6ebbbb1b870b1efda7f7b56838382ebea26dd957
godi15 <godi15@ru.is>
2019-12-06 11:19:36 +0000

    jkdjd

--- models/VoyagesModel.py
+++ models/VoyagesModel.py
@@ -6,9 +6,12 @@ class VoyagesModel():
         self.airplaneID = airplaneID
         self.crew_list = []
 
-    def csv_to_string(self):
+    def csv_voyage_to_string(self):
         return f"{self.time},{self.destination},{self.airplaneID}"
     
     def assign_crew_to_voyage(self, captain, copilot, fsm, fa1, fa2):
         staff_list = [captain, copilot, fsm, fa1, fa2]
         self.crew.append(staff_list)
+
+    def csv_voyage_with_crew_to_string(self):
+        return f"{self.time},{self.destination},{self.airplaneID}, {self.captain}, {self.copilot}, {self.fsm}, {self.fa1}, {self.fa2}"
