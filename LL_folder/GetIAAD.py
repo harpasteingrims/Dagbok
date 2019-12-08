54f58a0ae8cfc4339c325981cde4c59aede97f51
Harpa Steingrimsdottir <harpasteingrims@Harpas-MacBook.lan>
2019-12-08 18:11:51 +0000

    laga

--- LL_folder/GetIAAD.py
+++ LL_folder/GetIAAD.py
@@ -5,12 +5,15 @@ class GetIAAD():
         self.ioapi = ioapi
 
     def list_available_emp_by_date(self, user_input_date):
-        #employee_list = self.ioapi.get_list_of_all_employees()
+        employee_list = self.ioapi.get_list_of_all_employees()
         #voyage_list = self.list_voyages_status_by_date(self, user_input_date)
+        available_employees_list = []
         voyage_list = self.ioapi.get_all_voyages_list()
-
-        for voyage_ob in voyage_list:
-            pass
+        i = 0
+        for employee_ob in employee_list:
+            if employee_ob.name not in self.list_unavailable_emp_by_date(self, user_input_date)[i]:
+                available_employees_list.append(employee_ob.name)
+                i += 1
 
         #for employee in employee_list:
             #available_emps_for_selected_day = []
@@ -28,15 +31,16 @@ class GetIAAD():
             unavailable_employees_list = []
             date = voyage_ob.date
             parsed_date = dateutil.parser.parse(date)
-            if user_input_date.year == parsed_date.year and user_input_date.month == parsed_date.month and user_input_date.day == parsed_date.day:
-                unavailable_employees_list.append([voyage_ob.captain, voyage_ob.copilot, voyage_ob.fsm, voyage_ob.fa1, voyage_ob.fa2])
+            if user_input_date.year == parsed_date.year and user_input_date.month == parsed_date.month and user_input_date.day == parsed_date.day: #Hérna þyrftum við líka að tékka hvort almennt það eru starfsmenn á vélinni
+                unavailable_employees_list.append([voyage_ob.captain, voyage_ob.copilot, voyage_ob.fsm, voyage_ob.fa1, voyage_ob.fa2, voyage_ob.arriving_at])
+
+            return unavailable_employees_list
 
         #for employee in employee_list:
         #    available_emps_for_selected_day = []
             #if employee in (hvernig á ég að athuga allt staff á öllum ferðunum í listanum af objectum?)
          #       available_emps_for_selected_day.append(employee)
         #return available_emps_for_selected_day
-        pass
 
     def list_airplane_status_by_date(self, user_input_date):
         #airplane_list = self.ioapi.get_airplane_list
