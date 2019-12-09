6471b2827e6750acc6a34f74cc1074347fb0befc
Hallmar <schramhallmar@gmail.com>
2019-12-09 11:45:22 +0000

    breyta voyagemodel

--- hallmar_test.py
+++ hallmar_test.py
@@ -3,3 +3,4 @@ from IO_folder.GetIO import GetIO
 
 test = GetIO
 test.load_all_voyages_with_crew()
+list_of_list = [[]]
--- models/VoyagesModel.py
+++ models/VoyagesModel.py
@@ -1,13 +1,14 @@
 
 class VoyagesModel():
-    def __init__(self, date, destination, aircraftID, crew_list = []):
-        self.date = date #hér erum við búin í LL-layer að nota datetime þar sem þetta formattast saman í year,month,day,hour,minute
+    def __init__(self, departure_time, arrival_time, destination, aircraftID, crew_list = []):
+        self.departure_time = departure_time #hér erum við búin í LL-layer að nota datetime þar sem þetta formattast saman í year,month,day,hour,minute
+        self.arrival_time = arrival_time
         self.destination = destination
         self.aircraftID = aircraftID
         self.crew_list = []
 
     def csv_voyage_to_string(self):
-        return f"{self.date},{self.destination},{self.aircraftID}"
+        return f"{self.departure_time},{self.arrival_time},{self.destination},{self.aircraftID}"
     
     def assign_crew_to_voyage(self, captain, copilot, fsm, fa1, fa2):
         staff_list = [captain, copilot, fsm, fa1, fa2]
@@ -15,4 +16,4 @@ class VoyagesModel():
         return self.crew_list
 
     def csv_voyage_with_crew_to_string(self):
-        return f"{self.date}, {self.destination}, {self.aircraftID}, {self.crew_list[0]}, {self.crew_list[1]}, {self.crew_list[2]}, {self.crew_list[3]}, {self.crew_list[4]}, {self.crew_list[5]}"
+        return f"{self.departure_time},{self.arrival_time}, {self.destination}, {self.aircraftID}, {self.crew_list[0]}, {self.crew_list[1]}, {self.crew_list[2]}, {self.crew_list[3]}, {self.crew_list[4]}, {self.crew_list[5]}"
