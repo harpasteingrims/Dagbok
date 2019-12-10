630682fb64a531b1df86e2f71d35d7ce37d4b647
viktoriainga <viktoriainga@gmail.com>
2019-12-10 20:15:19 +0000

    jddj

--- IO_folder/CreateIO.py
+++ IO_folder/CreateIO.py
@@ -10,29 +10,22 @@ from models.DestinationsModel import DestinationsModel
 class CreateIO():
     def store_pilot(self, new_pilot):
         with open('csv_files\Pilots.csv', 'a') as f:
-            fieldnames = ["ssn","name","role","rank","plane_license","address","mobilenumber","email"]
-            print(new_pilot)
-            writer = csv.DictWriter(f, fieldnames=fieldnames)
-            writer.writerow(new_pilot.to_csv_string())
+            f.write(new_pilot.to_csv_string())
 
     def store_cabincrew(self, new_cabincrew):
         with open('csv_files\CabinCrew.csv', 'a') as f:
-            writer = csv.writer(f)
-            writer.writerow(new_cabincrew.to_csv_string())
+            f.write(new_cabincrew.to_csv_string())
         
     def store_airplane(self, new_airplane):
         with open('csv_files\Aircraft.csv', 'a') as f:
-            writer = csv.writer(f)
-            writer.writerow(new_airplane.to_csv_string())
+             f.write(new_airplane.to_csv_string())
 
     def store_destination(self, new_destination):
         with open('csv_files\Destinations.csv', 'a') as f:
-            writer = csv.writer(f)
-            writer.writerow(new_destination.to_csv_string())
+             f.write(new_destination.to_csv_string())
 
     def store_voyage(self, new_voyage):
         with open('csv_files\Flights.csv', 'a') as f:
-            writer = csv.writer(f)
-            writer.writerow(new_voyage.to_csv_string())
+             f.write(new_voyage.to_csv_string())
 
 
