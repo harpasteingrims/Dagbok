5bb33c36fb6dc927d027d30293bccc8810edec58
harpasteingrims <harpastein@gmail.com>
2019-12-10 10:14:38 +0000

    laga

--- __main__.py
+++ __main__.py
@@ -1,17 +1,9 @@
-#Þetta er rotarskrain
-
-
-# Arnar: látið þessa skrá bara heita __main__.py og hafið hana ekki
-# í sér möppu heldur í möppunni sem ER raunverulega rótin á verkefninu
-# (þeas í möppunni  VERKLEGT-1-verkefni)
-
 from UI_folder.UImanager import UImanager
 import dateutil.parser
 
 def main():
     ui = UImanager()
     ui.mainmenuUI()
-    #búum til eitt fall í mananger sem er þá run og þar verður self.
 
 if __name__ == '__main__':
     main()
