b962ad397138f10b6aba8ffc335a147316734aa1
viktoriainga <viktoriainga@gmail.com>
2019-12-11 17:25:30 +0000

    jdjd

--- UI_folder/AirplanesUI.py
+++ UI_folder/AirplanesUI.py
@@ -45,7 +45,7 @@ class AirplanesUI():
         for airplane_ob in airplanes_ob_list:
             print(f"\n{airplane_ob.planeID},{airplane_ob.airplane_type},{airplane_ob.manufacturer},{airplane_ob.seat_amount}")
 
-        print("B Back\n")
+        print("\nB Back\n")
 
         action_str = self.choose_action()
 
@@ -62,10 +62,10 @@ class AirplanesUI():
         print(self.LENGTH_STAR * "*")
         print("CREATE A NEW AIRPLANE \n")
 
-        airplane_id = self.AirplanesUI.get_airplane_id
-        airplane_type = self.AirplanesUI.get_airplane_type
-        manufacturer = self.AirplanesUI.get_manufacturer
-        seat_amount = self.AirplanesUI.get_seat_amount
+        airplane_id = self.get_airplane_id()
+        airplane_type = self.get_airplane_type()
+        manufacturer = self.get_manufacturer()
+        seat_amount = self.get_seat_amount()
         
         print("\nS Save \nB Back\n")
 
@@ -75,7 +75,7 @@ class AirplanesUI():
             #Takes the info and adds it to the airplane list
             new_airplane_object = AirplanesModel(airplane_id, airplane_type, manufacturer, seat_amount)
             self.llapi.create_new_airplane(new_airplane_object)
-            print(f"Airplane {new_airplane_object.airplane_id} successfully created\n")
+            print(f"Airplane {new_airplane_object, airplane_id} successfully created\n")
             return
 
         elif action_str == "b":
@@ -97,7 +97,7 @@ class AirplanesUI():
             self.get_airplane_id()
 
     def get_airplane_type(self):
-        airplane_type = input("Enter airplane type")
+        airplane_type = input("Enter airplane type: ")
         return airplane_type
 
     def get_manufacturer(self):
