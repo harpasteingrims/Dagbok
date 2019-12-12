ee5c5d7a1b8db5fcc4f6a7f06377da23a6a2da82
godi15 <godi15@ru.is>
2019-12-12 20:35:04 +0000

    laga

--- UI_folder/EmployeesUI.py
+++ UI_folder/EmployeesUI.py
@@ -420,6 +420,7 @@ class EmployeesUI():
             elif action_str == "b":
                 return
         
+        
     def get_date_from(self):
         print(self.LENGTH_STAR * "*")
         print("Enter date from\n")
