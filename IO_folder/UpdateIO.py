734070d65a89ac30e54adb8eec21ac005b8ef8e7
viktoriainga <viktoriainga@gmail.com>
2019-12-12 16:05:10 +0000

    laga

--- IO_folder/UpdateIO.py
+++ IO_folder/UpdateIO.py
@@ -40,7 +40,7 @@ class UpdateIO:
         ''' Updates a pilot '''
         pilot_list = GetIO.load_all_pilots(self)
         with open("./csv_files/Pilots.csv","w", encoding= "utf8", newline="") as csvfile:
-            fieldnames = ["ssn", " name", " role" ," rank" , " plane license" ," address" , " mobile number" , " email\n"]
+            fieldnames = ["ssn", " name", " role" ," rank" , " plane license" ," address" , " mobile number" , " email"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
 
@@ -54,7 +54,7 @@ class UpdateIO:
     def update_cabincrew(self, update_cabincrew):
         cabincrew_list = GetIO.load_all_cabincrew(self)
         with open("./csv_files/CabinCrew.csv","w", encoding= "utf8", newline="") as csvfile:
-            fieldnames = [" ssn", " name", " role", " rank", " address", " mobile number", " email\n"]
+            fieldnames = [" ssn", " name", " role", " rank", " address", " mobile number", "email"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
 
