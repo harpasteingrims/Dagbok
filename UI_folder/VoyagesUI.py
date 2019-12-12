84f937a30d01d8f9455e46fa0371e4b1dce1a3b1
godi15 <godi15@ru.is>
2019-12-12 23:30:14 +0000

    laga

--- UI_folder/VoyagesUI.py
+++ UI_folder/VoyagesUI.py
@@ -293,6 +293,8 @@ class VoyagesUI():
             crew_list = [captain_ob.ssn, copilot_ob.ssn, senior_cabincrew_member_ob.ssn, cabincrew_member_1_ob.ssn, cabincrew_member_2_ob.ssn]
             updated_voyage_ob = VoyagesModel(voyage_ob.departure_time, voyage_ob.destination, voyage_ob.aircraftID, voyage_ob.arrival_time, crew_list, voyage_ob.outbound_flight_num, voyage_ob.return_flight_num, voyage_ob.return_departure_time, voyage_ob.return_arrival_time)
             self.llapi.update_voyage(updated_voyage_ob)
+        
+            prrint("Staff assigned to voyage!")
 
 
     def show_not_staffed_voyages(self):
